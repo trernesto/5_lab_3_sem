7501d9d78126b03fdc1931a21ea0c04d79ea2b58
Er <Ernest@sobaka.ru>
2022-11-26 08:40:04 +0500

    Added "find regex" function

--- 5_lab_3_sem/Data.cs
+++ 5_lab_3_sem/Data.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,7 +13,12 @@ namespace _5_lab_3_sem
     {
         public string Text { get; set; }
         public string FileName { get; set; }
-        System.Text.RegularExpressions.Match Match { get; set; }
+        public System.Text.RegularExpressions.Match Match { get; set; }
+
+        internal void Find(string text)
+        {
+            this.Match = Regex.Match(this.Text, text); //text - regex
+        }
 
         internal void ReadFromFile(string fileName)
         {
--- 5_lab_3_sem/MainForm.cs
+++ 5_lab_3_sem/MainForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -23,10 +24,35 @@ namespace _5_lab_3_sem
             OpenFileDialog dlg = new OpenFileDialog();
             dlg.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
             dlg.FilterIndex = 1;
-            DialogResult res = dlg.ShowDialog(); 
+            DialogResult res = dlg.ShowDialog();
             if (res == DialogResult.OK)
             { data.ReadFromFile(dlg.FileName); }
             richTextBox1.Text = data.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data.Find(textBox1.Text);
+            this.ShowMatch();
+        }
+
+        private void ShowMatch()
+        {
+            Match m = data.Match; // получить m из data, добавить using
+            if (m != null && m.Success)
+            {
+                richTextBox1.SelectionBackColor = Color.White; // сброс подсветки
+                richTextBox1.SelectionStart = m.Index;
+                // начало - место, на котором
+                // в строке найдено регулярное выражение
+                richTextBox1.SelectionLength = m.Value.Length;
+                // длина найденного фрагмента
+                richTextBox1.ScrollToCaret(); // прокрутка на выделенное место
+                richTextBox1.SelectionBackColor = Color.Yellow; // подсветка
+
+                richTextBox2.Text = $"Найдено[{m.Index}]: ##{m.Value}##\n";
+
+            }
         }
     }
 }
